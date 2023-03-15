bfe3f820503dcb8912dec255ed74898d1e43393b
saiful-aromax <saiful.aromax@gmail.com>
2023-03-15 02:35:18 +0000

    update

--- mysite/mlpr_app/views.py
+++ mysite/mlpr_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from numpy import array
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 # from django.http import HttpResponse
 from . models import SearchLog
 # from . raw_query import raw_query
@@ -8,9 +10,20 @@ from . models import SearchLog
 # Create your views here.
 
 
+# def home(request):
+#     AroVar = {"alpha": "A", "bita": "B", "gamma": 'C'}
+#     return render(request, 'mlpr_app/home.html', {"AroVar": AroVar})
+
 def home(request):
-    AroVar = {"alpha": "A", "bita": "B", "gamma": 'C'}
-    return render(request, 'mlpr_app/home.html', {"AroVar": AroVar})
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'mlpr_app/home.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'mlpr_app/home.html')
 
 
 def about(request):
@@ -21,6 +34,7 @@ def about(request):
 def contact(request):
     commodities = []
     return render(request, 'mlpr_app/contact.html', {"commodities": commodities})
+
 
 def dis(request):
     array_list = array(["Saiful", "Aromax", 3, 4])
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -133,6 +133,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
