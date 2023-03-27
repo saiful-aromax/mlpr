5679da5fc83b6f181ea6fd5f03a9beffa9d9f2e2
saiful-aromax <saiful.aromax@gmail.com>
2023-03-27 06:28:25 +0100

    update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--c=oh5!9s=q7r8i=b_*ccfb7bv#)r1#8$m00@%-73a+emsn$!=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
